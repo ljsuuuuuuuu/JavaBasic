991ec8f53d4b6a5aebbb114bef6de22d470917bd
ljsuuuuuuuu <162402306+ljsuuuuuuuu@users.noreply.github.com>
2024-07-02 09:51:19 +0900

    Delete Java_basic directory

--- Java_basic/chap01/src/sec01/Hello.java
+++
@@ -1,7 +0,0 @@
-package sec01;
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
-}
--- Java_basic/chap04/src/print/Print03.java
+++
@@ -1,9 +0,0 @@
-package print;
-
-public class Print03 {
-	public static void main(String[] args) {
-		String myName = "이지수";
-		int myAge = 23;
-		System.out.println("이름 :\t" + myName + "\n나이 :\t" + myAge);
-	}
-}
